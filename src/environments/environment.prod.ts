dc5aefe5509b21c88a78b9541ac798eb9ef68566
Nitesh Soni <niteshmitrc@gmail.com>
2020-07-06 11:39:20 +0530

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDzANynJkkvBLLNQ08vWsSjeIPwbaEt6go",
+    authDomain: "github-search-27af5.firebaseapp.com",
+    databaseURL: "https://github-search-27af5.firebaseio.com",
+    projectId: "github-search-27af5",
+    storageBucket: "github-search-27af5.appspot.com",
+    messagingSenderId: "689051572707",
+    appId: "1:689051572707:web:eed037570f394b454ea8d6",
+    measurementId: "G-REHFGRSKE0"
+  }
 };
